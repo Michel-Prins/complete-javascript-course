0b379f39a4237f0f8de13682da6048d266e6a225
Michel Prins <michel.prins@philips.com>
2024-11-29 16:56:17 +0100

    Dag 3: Fundamentals Part 1 voltooid

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -17,13 +17,16 @@ let country = 'Netherlands';
 let continent = 'Europe';
 let population = '18';
 
+//let kan je tenminste als waarde nog laten veranderen...
+//Const kan je de hele waarde niet meer veranderen???
+
 console.log(country);
 console.log(continent)
 console.log(population)
 
 //Excirsise 2
 const isIsland = false;
-const language = 'Dutch';
+let language = 'Dutch';
 
 console.log('Is it a Island?: ' + isIsland);
 console.log('Language spoken: ' + language);
@@ -43,6 +46,12 @@ console.log('Our population greater than Average? ' + (population > avgPopulatio
 const sentence = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
 console.log(sentence);
 
+const sentenceTemplateLiterals = `The ${country} is in ${continent}, and its ${population} million people speak ${language}.`;
+console.log(sentenceTemplateLiterals);
+
+if (population < avgPopulation) {
+    console.log(`The ${country} population is ${avgPopulation - population} million below avarage.`)
+}
 
 //Challenge 1
 const massMark = 78;
@@ -65,6 +74,7 @@ console.log(michelNew)
 
 const age = 15;
 
+//Tussen de { } noem je een block
 //IF ELSE Control Structure
 if (age >= 18) {
     console.log('Jeej oud genoeg 🎉')
@@ -72,3 +82,175 @@ if (age >= 18) {
     const yearsLeft = 18 - age;
     console.log(`Je bent niet oud genoeg, je moet nog ${yearsLeft} jaren wachten! 😒`)
 }
+
+//Challange 2
+if (bmiMark > bmiJohn) {
+    console.log(`Mark's BMI (${bmiMark}) is higher than John's (${bmiJohn})!`)
+} else {
+    console.log(`John's BMI (${bmiJohn}) is higher than Mark's (${bmiMark})!`)
+}
+
+console.log('9' - '5'); // -> ?4
+console.log('19' - '13' + '17'); // -> ?617
+console.log('19' - '13' + 17); // -> ?23
+console.log('123' < 57); // -> ?false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // -> ?1143
+
+//TYPE Conversion
+//Convert String to Numner: Number("18");
+console.log(Number("18"));
+
+//TYPE COERCION
+console.log('You are ' + 19 + ' old.')
+console.log('You are ' + String(19) + ' old.')  //Zoals in een andere taal zoasl c# zou je deze moeten gebruiken.
+//Alle calculation operators wordt alles naar een int gemaakt, met een + word de string concatenated
+
+//5 falsy values: 0, '', undefined (let money), null, NaN
+//Voorbeeld:
+
+const money = 0;    //Dit wordt dus een false!
+if (money) {
+    console.log(`Dont spend it all! 🤑`);
+} else {
+    console.log(`You should gat a job!`);
+}
+
+//Equiality operators
+//== uses type coercion (loose)
+//=== does not use coercion (strict) => is the better used version
+const ageEquiality = 18;
+if (ageEquiality === 18) console.log(`You just became an adult.`);
+
+//const favouriteNumber = prompt(`Whats your favourite number?`);
+//console.log(`Your favourite number: ${favouriteNumber}`);
+
+// if (favouriteNumber == 23) { // '23' == 23
+//     console.log(`Cool! 23 is an amazing number!`);
+// }
+
+//Better use!
+//const favouriteNumber2 = Number(prompt(`Whats your favourite number?`));
+
+// if (favouriteNumber2 === 23) { // '23' == 23
+//     console.log(`Cool! 23 is an amazing number!`);
+// }
+
+// !== strict version for not is |  != loose version
+
+
+// const numNeighbours = Number(prompt('How many neighbour countries does your contry have?'));
+
+// if (numNeighbours === 1) {
+//     console.log(`Only 1 border!`);
+// } else if (numNeighbours > 1) {
+//     console.log(`More than 1 border!`);
+// } else console.log(`No borders!`);
+
+const hasDriverLicense = true;
+const hasGoodVision = true;
+
+console.log(hasDriverLicense && hasGoodVision)
+
+// || = OR
+// && = AND
+// ! = inverter value
+//TIP! Ctrl + / = comment
+
+if ((language === 'English') && (population < 50) && !isIsland) {
+    console.log(`You should live in ${country} :)`);
+} else { console.log(`${country} does not meet your criteria :(`); }
+
+//Challange 3
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+
+if (scoreKoalas === scoreDolphins) {
+    console.log("Both win the trophy");
+} else if (scoreDolphins > scoreKoalas) {
+    console.log("Dolphins win the trophy");
+} else { console.log("Koalas win the trophy"); }
+
+
+//Switches => Beter leesbaar! Wordt niet zoveel meer gebruikt...
+const day = 'monday';
+
+switch (day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;  //zonder de break zou hij verder gaan met de code hieronder.
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':       //wo en do hebben hetzelfde gedrag
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':        //za en zo zijn ook gelijk hier... Kijk goed naar de break!
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+        break;
+    default:
+        console.log('Not a valid day!');
+}
+
+
+//Dit is hetzelfde als met een case...
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend :D');
+} else {
+    console.log('Not a valid day!');
+}
+
+//Conditianal formatter/Operator (Ternary)!
+//Sneller dan if else!
+age >= 18 ? console.log(`Jeej, zuipen!`) : console.log(`Jammer joh, lekker aan de gemeente pils!`);
+//Boven is een voorbeel, als volgt wrdt gebruikt
+
+const drink = age >= 18 ? 'Beer' : 'Water';
+console.log(drink);
+
+language = 'chinese';
+
+switch (language) {
+    case 'chinese':
+    case 'manderin':
+        console.log('MOST number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'english':
+        console.log('3rd place');
+        break;
+    case 'hindi':
+        console.log('Number 4');
+        break;
+    case 'arabic':
+        console.log('5th most spoken language');
+        break;
+    default:
+        console.log('Great language too :D');
+}
+
+const isAvgPopulation = population > avgPopulation ? 'above' : 'below';
+console.log(`${country}'s population is ${isAvgPopulation} average`);
+// Even better!!!
+console.log(`${country}'s population is ${population > avgPopulation ? 'above' : 'below'} average`);
+
+//Challange 4
+const bill = 430;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${tip + bill}`)
