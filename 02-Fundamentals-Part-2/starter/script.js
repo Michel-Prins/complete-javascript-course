e9b133c884a33d677abca774fee7b804810011b4
Michel Prins <michel.prins@philips.com>
2024-12-04 13:11:17 +0100

    Day4: functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -76,3 +76,98 @@ const netherlandsPop2 = percentageOfWorld2(18);
 const germanyPop2 = percentageOfWorld2(84);
 
 console.log(chinaPop2.toFixed(2), netherlandsPop2.toFixed(2), germanyPop2.toFixed(2));
+
+
+// Arrow functions
+
+const calcAge3 = birthYeah => 2037 - birthYeah;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYeah, firstName) => {
+    const age = 2037 - birthYeah;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
+
+
+// Functions Calling Other Functions
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+}
+console.log(fruitProcessor(2, 3));
+
+
+// Review functions...
+//function expression
+const calcAge_full = function (birthYear) {
+    return new Date().getFullYear() - birthYear; //new Date().getFullYear() gives the current year.
+}
+
+//function in function
+const yearsUntilRetirement_full = function (birthYear, firstName) {
+    const age = calcAge_full(birthYear)
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired 🎉`)
+        return -1;  //standaard nummer in programming? Betekend dat het geen meaning heeft.
+    }
+}
+
+console.log(yearsUntilRetirement_full(1987, 'Michel'));
+
+//opdracht
+const describePopulation = function (country, population) {
+    const popPercentageWorld = percentageOfWorld1(population).toFixed(1);
+    console.log(`${country} has ${population} people, wich is about ${popPercentageWorld} of the world.`);
+}
+
+describePopulation('Netherlands', 18);
+describePopulation('Germany', 84);
+describePopulation('China', 1441);
+
+/*
+CHALLENGE #1
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+
+A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
+*/
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+    if (avgDolphins / 2 > avgKoalas) {
+        console.log(`Dolphins win ${avgDolphins} vs ${avgKoalas}`);
+    } else if (avgKoalas / 2 > avgDolphins) {
+        console.log(`Koalos win ${avgKoalas} vs ${avgDolphins}`);
+    } else {
+        console.log(`No team wins...`);
+    }
+}
+
+const scoreDolphins1 = calcAverage(44, 23, 71);
+const scoreKoalas1 = calcAverage(65, 54, 49);
+
+const scoreDolphins2 = calcAverage(85, 54, 41);
+const scoreKoalas2 = calcAverage(23, 34, 27);
+
+checkWinner(scoreDolphins1, scoreKoalas1);
+checkWinner(scoreDolphins2, scoreKoalas2);
