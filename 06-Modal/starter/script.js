6d8cd14cc11255b21f78b42609860912e0cb6f39
Michel Prins <michel.prins@philips.com>
2024-12-13 15:13:29 +0100

    Dag 11:  project 2 Model

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal'); //definitie van de X in de modal-window
+const btnsOpenModal = document.querySelectorAll('.show-modal'); //queryselector select alleen de eerste..
+
+console.log(btnsOpenModal);
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal); //function moet je dan niet aanroepen met de arguments.. ()
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  //Als je hem wel een argument geeft, kun je daar wat mee doen...
+  //   console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal(); //Nu moet je hem wel als functie aanroepen
+  }
+});
