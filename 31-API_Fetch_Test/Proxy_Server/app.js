da506f0f1559181680ffae86c28f2edf620321b0
Michel Prins <michel.prins@philips.com>
2024-12-18 16:06:45 +0100

    API test met HydraX

---
+++ 31-API_Fetch_Test/Proxy_Server/app.js
@@ -0,0 +1,90 @@
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.get(`/`, (req, res) => {
+//   res.send(`Started Working, Express!`);
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server listening at port: ${port}`);
+// });
+
+//==========================================================================================
+//==========================================================================================
+// Andere bron:
+// const express = require('express');
+// const cors = require('cors');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// const app = express();
+// app.use(cors());
+// app.use(
+//   createProxyMiddleware({
+//     router: req => new URL(req.path.substring(1)),
+//     pathRewrite: (path, req) => new URL(req.path.substring(1)).pathname,
+//     changeOrigin: true,
+//     logger: console,
+//   })
+// );
+
+// app.listen(8088, () => {
+//   console.info('proxy server is running on port 8088');
+// });
+
+//==========================================================================================
+//==========================================================================================
+// to start this server: Type in console: node app.js
+
+var express = require('express'),
+  request = require('request'),
+  bodyParser = require('body-parser'),
+  app = express();
+
+var myLimit = typeof process.argv[2] != 'undefined' ? process.argv[2] : '100kb';
+console.log('Using limit: ', myLimit);
+
+app.use(bodyParser.json({ limit: myLimit }));
+
+app.all('*', function (req, res, next) {
+  // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    req.header('access-control-request-headers')
+  );
+
+  if (req.method === 'OPTIONS') {
+    // CORS Preflight
+    res.send();
+  } else {
+    var targetURL = req.header('Target-URL'); // Target-URL ie. https://example.com or http://example.com
+    if (!targetURL) {
+      res.send(500, {
+        error: 'There is no Target-Endpoint header in the request',
+      });
+      return;
+    }
+    request(
+      {
+        url: targetURL + req.url,
+        method: req.method,
+        json: req.body,
+        headers: { Authorization: req.header('Authorization') },
+      },
+      function (error, response, body) {
+        if (error) {
+          console.error('error: ' + response.statusCode);
+        }
+        //                console.log(body);
+      }
+    ).pipe(res);
+  }
+});
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), function () {
+  console.log('Proxy server listening on port ' + app.get('port'));
+});
--- 31-API_Fetch_Test/index.js
+++ 31-API_Fetch_Test/index.js
@@ -1,20 +1,20 @@
-fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
   //   .then((response) => console.log(response))
-  .then((response) => {
+  .then(response => {
     if (!response.ok) {
       throw new Error(`Could not fetch resource`);
     }
     return response.json();
   })
-  //   .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((error) => console.error(error));
+  // .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
 
 // Fetch in een try catch block
 async function fetchData() {
   try {
     const pokemonName = document
-      .getElementById("pokemonName")
+      .getElementById('pokemonName')
       .value.toLowerCase();
 
     const response = await fetch(
@@ -27,10 +27,10 @@ async function fetchData() {
 
     const data = await response.json();
     const pokemonSprite = data.sprites.front_default;
-    const imgElement = document.getElementById("pokemonSprite");
+    const imgElement = document.getElementById('pokemonSprite');
 
     imgElement.src = pokemonSprite;
-    imgElement.style.display = "block";
+    imgElement.style.display = 'block';
 
     console.log(data);
   } catch (error) {
@@ -38,4 +38,47 @@ async function fetchData() {
   }
 }
 
-// fetchData();
+//==========================================================================================
+//==========================================================================================
+
+// Dit zou denk ik in een express iets moeten draaien...
+// const cors = require('cors');
+// const corsOptions = {
+//   origin: '*',
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
+// app.use(cors(corsOptions)); // Use this after the variable declaration
+
+let username = 'drachten';
+let password = 'Drachten@1!';
+let auth = btoa(`${username}:${password}`);
+
+// // fetchData();
+// fetch(
+//   'https://cors-anywhere.herokuapp.com/?https://dfhs0012.dtnfd.nl-016.lan.philips.com:8080/meta?X-Access-Id=00011475',
+//   {
+//     headers: { Authorization: `Basic ${auth}` },
+//   }
+// ) //   .then((response) => console.log(response))
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new error(`Could not fetch resource`);
+//     }
+//     return response.json();
+//   })
+//   .then(response => response.json())
+//   .then(json => console.log(JSON.stringify(json)))
+//   .then(data => console.log(data));
+// // .catch(error => console.error(error));
+
+// // {
+// //   headers: { Authorization: 'Bearer {token}' },
+// // }
+
+fetch(
+  'http://localhost:3000/https://dfhs0012.dtnfd.nl-016.lan.philips.com:8080/meta?X-Access-Id=00011475',
+  {
+    headers: { Authorization: `Basic ${auth}` },
+  }
+).then(response => console.log(response));
